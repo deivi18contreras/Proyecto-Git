ffe8505a5447f04adfc85db5c4881fe6a965777a
deivi18contreras <deivibogoya@gmail.com>
2026-03-27 20:33:06 -0500

    Terminado el esquema de mongoose #14

--- backend/database/config.js
+++ backend/database/config.js
@@ -5,7 +5,7 @@ export const dbConnection = async () => {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('✅ Base de datos online - Colegio San José');
     } catch (error) {
-        console.error('❌ Error al iniciar la base de datos:', error);
+        console.error('❌ Error al iniciar la base de datos:❌', error);
         throw new Error('Error a la hora de iniciar la base de datos');
     }
 };
